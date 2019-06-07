ec139a880a088a961946cc81fd2e850e71dc762e
Harvey Chen <harvey.chen@transport.nsw.gov.au>
2019-06-07 14:11:40 +1000

    Add project files.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hello.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            //Message = "Your application description page.";
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+    }
+}
